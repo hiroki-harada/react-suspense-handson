805a48432e47bf7ea91c44b94c912e69fe4b155a
hiroki-harada <poketgame.0604@gmail.com>
2022-06-05 00:27:57 +0000

    [add] finish chap 7

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,36 @@
 import { Suspense, useState } from 'react'
-import { DataLoader1, DataLoader2 } from './Dataloader'
+import { DataLoader } from './Dataloader'
 import './App.css'
-import { RenderingNotifier } from './RenderingNotifier';
+import { Loadable } from "./Loadable";
+
+const sleep = (ms: number) => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function fetchData1(): Promise<string> {
+  await sleep(Math.floor(Math.random() * 1000));
+  return `Hello, ${(Math.random() * 1000).toFixed(0)}`;
+}
+
 
 function App() {
-  const [count, setConunt] = useState(0);
+  const [data1] = useState(() => new Loadable(fetchData1()));
+  const [data2] = useState(() => new Loadable(fetchData1()));
+  const [data3] = useState(() => new Loadable(fetchData1()));
   return (
     <div className="text-center">
       <h1 className="text-2xl">React App!</h1>
-      <RenderingNotifier name="outside-Suspense"/>
       <Suspense fallback={<p>Loading...</p>}>
-        <DataLoader1/>
-        <DataLoader2/>
+        <DataLoader data={data1} />
+      </Suspense>
+      <Suspense fallback={<p>Loading...</p>}>
+        <DataLoader data={data2} />
+      </Suspense>
+      <Suspense fallback={<p>Loading...</p>}>
+        <DataLoader data={data3} />
       </Suspense>
     </div>
-  )
+  );
 }
 
 export default App
--- src/Dataloader.tsx
+++ src/Dataloader.tsx
@@ -1,42 +1,15 @@
+import { Loadable } from "./Loadable";
 
-const dataMap: Map<string, string> = new Map();
-
-const sleep = (ms: number) => {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-async function fetchData1(): Promise<string> {
-    await sleep(1000);
-    return `Hello, ${(Math.random() * 1000).toFixed(0)}`;
-}
-
-
-function useData<T>(cacheKey: string, fetch: () => Promise<T>): T {
-  const cachedData = dataMap.get(cacheKey) as T | undefined;
-  if (cachedData === undefined) {
-    throw fetch().then((d) => dataMap.set(cacheKey, d));
-  }
-  return cachedData;
-}
-
-const DataLoader1: React.VFC = () => {
-  const data = useData("DataLoader1", fetchData1);
+const DataLoader: React.VFC<{
+  data: Loadable<string>;
+}> = ({ data }) => {
+  const value = data.getOrThrow();
   return (
     <div>
-      <div>Data is {data}</div>
-    </div>
-  );
-};
-
-const DataLoader2: React.VFC = () => {
-  const data = useData("DataLoader2", fetchData1);
-  return (
-    <div>
-      <div>Data is {data}</div>
+      <div>Data is {value}</div>
     </div>
   );
 };
 
 
-
-export {DataLoader1, DataLoader2}
+export {DataLoader}
